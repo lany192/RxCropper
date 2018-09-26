a7d6a5b8dcd51a21b2c22c68cb5b94d2545b314c
Lany192 <lany192@qq.com>
2018-09-26 21:57:23 +0800

    update

--- sample/src/main/java/com/lany/cropper/sample/App.java
+++ sample/src/main/java/com/lany/cropper/sample/App.java
@@ -1,13 +1,13 @@
 package com.lany.cropper.sample;
 
-import android.app.Application;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.lany.box.BaseApp;
 import com.lany.picker.RxPicker;
 import com.lany.picker.utils.ImageLoader;
 
-public class App extends Application {
+public class App extends BaseApp {
 
     @Override
     public void onCreate() {
@@ -16,7 +16,12 @@ public class App extends Application {
 
             @Override
             public void display(ImageView imageView, String path, int width, int height) {
-                Glide.with(imageView.getContext()).load(path).error(R.drawable.ic_preview_image).centerCrop().override(width, height).into(imageView);
+                Glide.with(imageView.getContext())
+                        .load(path)
+                        .error(R.drawable.ic_preview_image)
+                        .centerCrop()
+                        .override(width, height)
+                        .into(imageView);
             }
         });
     }
--- sample/src/main/java/com/lany/cropper/sample/ResultActivity.java
+++ sample/src/main/java/com/lany/cropper/sample/ResultActivity.java
@@ -4,23 +4,25 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.lany.box.activity.BaseActivity;
 
-public class ResultActivity extends AppCompatActivity {
+
+public class ResultActivity extends BaseActivity {
     static Bitmap mImage;
     private ImageView imageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crop_result);
+    protected int getLayoutId() {
+        return R.layout.activity_crop_result;
+    }
 
+    @Override
+    protected void init(Bundle bundle) {
         imageView = findViewById(R.id.resultImageView);
         imageView.setBackgroundResource(R.drawable.backdrop);
 
