754ebf170327b7d7864ad780d3127d06ce422798
lyg <lany192@qq.com>
2019-05-21 11:32:12 +0800

    update

--- sample/src/main/java/com/lany/cropper/sample/Application.java
+++ sample/src/main/java/com/lany/cropper/sample/Application.java
@@ -1,12 +1,9 @@
 package com.lany.cropper.sample;
 
-import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.github.lany192.rxpicker.RxPicker;
-import com.github.lany192.rxpicker.utils.ImageLoader;
 
 public class Application extends android.app.Application {
 
@@ -15,8 +12,8 @@ public class Application extends android.app.Application {
         super.onCreate();
         RxPicker.of().init((imageView, path, width, height) -> {
             RequestOptions options = new RequestOptions()
-                    .placeholder(R.drawable.ic_preview_image)
-                    .error(R.drawable.ic_preview_image)
+                    .placeholder(R.drawable.ic_launcher)
+                    .error(R.drawable.ic_launcher)
                     .override(width, height)
                     .centerCrop()
                     .diskCacheStrategy(DiskCacheStrategy.NONE);
